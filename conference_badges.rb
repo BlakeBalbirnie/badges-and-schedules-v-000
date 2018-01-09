f9d24011660ac1c8022cfd3c5e6bd6495b214d13
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-09 15:11:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,8 @@ end
 def batch_badge_creator(badge_name)
   badge_name.map {|name| name = "Hello, my name is #{name}."}
 end
+
+def assign_rooms(assign_room)
+  room = 0
+  assign_room.map {|i| i = "Hello, #{i}! You'll be assigned to room #{room +=1}!"}
+end
