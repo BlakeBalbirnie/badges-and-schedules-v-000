58eaaef403700be8485f579f2fe962b44643a685
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-09 15:07:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,3 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
-
-
-  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-    array.each puts "Hello, my name is #{name}."
-    return badge_maker
-  end
-end
