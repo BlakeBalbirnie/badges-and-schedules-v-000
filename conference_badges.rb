f99f274d4e6abcb0e7fde7f387f70f40524804cc
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-09 15:02:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
 # Write your code here.
+def badge_maker
+  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  name.each do |color|
+    puts "Hello, my name is #{color}."
+  end
+end
